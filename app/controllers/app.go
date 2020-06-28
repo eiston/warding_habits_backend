970f95fee7646b4e3283bdc878f34ccfe1217b7e
bretsky <bretskyselby99@gmail.com>
2020-06-28 15:26:52 -0700

    Add some requests

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -12,6 +12,14 @@ type App struct {
 	*revel.Controller
 }
 
+type Point struct {
+	x int
+	y int
+	value int
+}
+
+const API_KEY = "RGAPI-ca13e0ef-8468-4771-a54d-40881a73496b"
+
 func (c App) Index() revel.Result {
 	return c.Render()
 }
@@ -43,4 +51,45 @@ func (c App) SummonerInfo(region string, name string) revel.Result {
 	data["data"] = result
 	fmt.Printf("%s\n",data)
 	return c.RenderJSON(data)
+}
+
+func (c App) getSummonerByName(region string, name string) map[string]interface{} {
+	var url = fmt.Sprintf("https://%s.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s?api_key=%s", region, name, API_KEY)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	return result
+}
+
+func (c App) getMatchlist(region string, accountId string, beginTime int) map[string]interface{} {
+	var url = fmt.Sprintf("https://%s.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s?beginTime=%d&api_key=%s", region, accountId, beginTime, API_KEY)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	return result
+}
+
+func (c App) WardData(region string, name string) revel.Result {
+	result := c.getSummonerByName(region, name)
+
+	accountId := result["data"]["accountId"]
+
+	matchList := c.getMatchlist(region, accountId, 0)
+
+	for _, match := range matchList {
+		
+	}
+
+
+
+
+	var data [max_length]([]Point)
 }
